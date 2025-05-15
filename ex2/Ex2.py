c856b46c573b3317bcecdcff64d6a2328b767d40
Aaron Reuven <roni.reuven@gmail.com>
2025-05-15 13:31:22 +0300

    Magic Square Problem class definition

--- ex2/Ex2.py
+++ ex2/Ex2.py
@@ -1,4 +1,95 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# magic square problem GA first commit
+
+class MagicSquareProblem:
+    def __init__(self, size, seed=None):
+        self.seed = seed
+        np.random.seed(self.seed)
+        self.size = size
+        self.sub_constant = size**2 + 1
+        self.sub_square_constant = 2 * self.sub_constant
+        self.constant = size * self.sub_constant / 2
+        self.square = np.random.permutation(range(1, size*size + 1)).reshape((size, size))
+
+    def fitness(self):
+        # get sums of columns and rows
+        cols_abs = np.abs(self.square.sum(axis=0) - self.constant).sum()
+        rows_abs = np.abs(self.square.sum(axis=1) - self.constant).sum()
+        # get major diagonals
+        diag1 = np.diag(self.square)
+        diag2 = np.diag(np.rot90(self.square, 1))
+        # get sums of major diagonals
+        diag1_abs = abs(diag1.sum() - self.constant)
+        diag2_abs = abs(diag2.sum() - self.constant)
+
+        if self.size % 2 == 1:
+            return sum([cols_abs, rows_abs, diag1_abs, diag2_abs])
+        else:
+            # gets pairs on major diagonals
+            pairs1 = sum([abs(diag1[i::self.size//2].sum() - self.sub_constant) for i in range(self.size//2)])
+            pairs2 = sum([abs(diag2[i::self.size//2].sum() - self.sub_constant) for i in range(self.size//2)])
+
+            # get sum of every 2x2 sub-square with wraparound
+            sub_squares_sum = sum(self._get_wrapped_2x2_subsquares())
+
+            # returns the sum of all the differences to their target; perfect square is fitness = 0
+            return sum([sub_squares_sum, diag1_abs, diag2_abs, pairs1, pairs2, cols_abs, rows_abs])
+
+    def _get_wrapped_2x2_subsquares(self):
+        # helper function to get sub-squares
+        subsquares = []
+        for i in range(self.size):
+            for j in range(self.size):
+                sub_square = self.square[np.ix_([i, (i + 1) % self.size], [j, (j + 1) % self.size])]
+                sub_square_sum = sub_square.sum()
+                sub_abs = abs(sub_square_sum - self.sub_square_constant)
+                subsquares.append(sub_abs)
+        return subsquares
+
+    # maybe define solver/learner here or create new class for it
+
+    # maybe add the genetic functions here
+    # while the GeneticAlgorithm class will run the class abstractly
+
+    def __eq__(self, other):
+        return self.fitness() == other.fitness()
+
+    def __ge__(self, other):
+        return self.fitness() >= other.fitness()
+
+    def __le__(self, other):
+        return self.fitness() <= other.fitness()
+
+    def __gt__(self, other):
+        return self.fitness() > other.fitness()
+
+    def __lt__(self, other):
+        return self.fitness() < other.fitness()
+
+    def __str__(self):
+        return self.square.__str__()
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class GeneticAlgorithm:
+    def __init__(self):
+        pass
+
+
+# couple of tests will delete later
+msp = MagicSquareProblem(3, 42)
+msp_2 = MagicSquareProblem(3, 39)
+msp_3 = MagicSquareProblem(3, 32)
+print(msp)
+print(msp_2)
+print(msp_3)
+print(msp.fitness())
+print(msp_2.fitness())
+print(msp_3.fitness())
+print(msp == msp_2)
+l = [msp, msp_2, msp_3]
+print(sorted(l, reverse=True))
